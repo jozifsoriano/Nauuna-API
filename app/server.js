1f3b0bc335a0ca86e2bf44a24deaf4a3acff3d1e
jozifsoriano <josephmsoriano@outlook.com>
2023-02-08 23:49:03 -0600

    Dockeize repo

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const upload = multer({dest:'uploads/'});
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
